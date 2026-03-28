fa0d139a2bf003a86d5e62e69eaf2a4dcf83d907
Judy Babinski <judysstudiomac@Judys-Mac-Studio.local>
2026-03-28 16:22:38 -0400

    Add Google Search Console verification tag to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="google-site-verification" content="klOovqtK6rEySEklPrmt18oXXirpLUgttlMzVs6SwYU" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
